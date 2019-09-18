66cfd3a3bdbb27c9b0a40a073408f682e3fe014d
Icermli <haohan@excelsia.io>
2019-09-18 18:24:44 +0800

    add readslow

---
+++ Chapter07/readslow.c
@@ -0,0 +1,21 @@
+/* readslow: keep reading, waiting for more */
+#include <stdlib.h>
+#include <unistd.h>
+#define SIZE 512 /* arbitrary */
+
+int main (argc, argv)
+    int argc;
+    char *argv[];
+{
+    char buf[SIZE];
+    int n;
+    int t = 10;
+
+    if (argc > 1) { t = atoi(argv[1]); }
+
+    for (;;) {
+        while ((n = read(0, buf, sizeof buf)) > 0)
+           write(1, buf, n);
+        sleep(t);
+    }
+}
